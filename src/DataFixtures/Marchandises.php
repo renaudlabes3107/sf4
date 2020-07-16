15c25b090618949980b44086a552326b5884b109
Renaud LABES <renaudlabes@gmail.com>
2020-07-16 14:39:23 +0200

    Création de  fixture  Marchandises

--- src/DataFixtures/Marchandises.php
+++ src/DataFixtures/Marchandises.php
@@ -3,9 +3,9 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class Marchandises extends Fixture
 {
     public function load(ObjectManager $manager)
     {
